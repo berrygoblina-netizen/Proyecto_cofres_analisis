0f70774096976713d0cb3f69fb0341a7e59ae9b5
Natalia Soledad Britos <berrygoblina@gmail.com>
2026-04-01 16:08:01 -0300

    action: commit

--- analisis/tools/generar_json.py
+++ analisis/tools/generar_json.py
@@ -24,4 +24,4 @@ with open(ruta_json, "w", encoding="utf-8") as f:
     json.dump(datos, f, indent=4, ensure_ascii=False)
 
 print("JSON generado")
-#python analisis/generar_json.py
+#python analisis/tools/generar_json.py
